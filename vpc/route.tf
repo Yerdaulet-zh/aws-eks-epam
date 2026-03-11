9de74329ac71f27be280711cd66b64096a499601
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-11 09:40:58 +0500

    add route table and assosiations

---
+++ vpc/route.tf
@@ -0,0 +1,49 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "${local.project_name}-public-rt"
+    Project = local.project_name
+  }
+
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name    = "${local.project_name}-private-rt"
+    Project = local.project_name
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = {
+    for key, value in locals.local.subnets : key => value
+    if value.public == true
+  }
+
+  subnet_id      = aws_subnet.managed[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  for_each = {
+    for key, value in local.subnets : key => value
+    if value.public == false
+  }
+
+  subnet_id      = aws_subnet.managed[each.key].id
+  route_table_id = aws_route_table.private.id
+}
