e5df3e2185ad8d08de3d1ca867d8f33b31213a67
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-11 09:23:56 +0500

    adjust subnets, now it include az a and b, private and public subnets as well as private subnet specifically for a storage related servers (aka redis, milvus and etc)

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,6 +1,13 @@
 locals {
-  region             = "eu-central-1"
-  project_name       = "epam-ansible"
-  vpc_cidr_block     = "10.0.0.0/16"
-  subnet_public_cidr = "10.0.1.0/24"
+  region         = "eu-central-1"
+  project_name   = "epam-ansible"
+  vpc_cidr_block = "10.1.0.0/16"
+
+  subnets = {
+    "public_a"  = { cidr = "10.1.1.0/24", az = "a", public = true, usage = "elb" }
+    "public_b"  = { cidr = "10.1.2.0/24", az = "b", public = true, usage = "elb" }
+    "private_a" = { cidr = "10.1.16.0/20", az = "a", public = false, usage = "internal-elb" }
+    "private_b" = { cidr = "10.1.32.0/20", az = "b", public = false, usage = "internal-elb" }
+    "db_a"      = { cidr = "10.1.50.0/24", az = "a", public = false, usage = "storage" }
+  }
 }
