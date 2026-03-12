bc84002201b288692ace508e066920ed564ea9ed
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-12 10:02:36 +0500

    a seperate route table for db subnets will not be created

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,7 +38,7 @@ module "vpc" {
 
   # RDS
   create_database_subnet_group           = true
-  create_database_subnet_route_table     = true  # enable internal vpc communication
+  create_database_subnet_route_table     = false # enable internal vpc communication
   create_database_internet_gateway_route = false # disable vpc internet access to rds subnets
   create_database_nat_gateway_route      = false
 
